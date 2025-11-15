8ccac914d34e6572a9972ee0a6e7b98ed828c892
landanfp <159138257+landanfp@users.noreply.github.com>
2025-11-15 10:37:02 +0330

    Update admins.py

--- bot/utilities/pyrofilters/admins.py
+++ bot/utilities/pyrofilters/admins.py
@@ -1,21 +1,42 @@
-# ruff: noqa: ARG001
-
+# bot/utilities/pyrofilters/admin.py file :
 from pyrogram import filters
 from pyrogram.client import Client
 from pyrogram.types import Message
 
 from bot.config import config
-from bot.options import options
 
 
-class AdminsFilter:
-    @staticmethod
-    def admin(
-        allow_global: bool = False,  # noqa: FBT001, FBT002
-    ) -> filters.Filter:
-        async def func(flt: None, client: Client, message: Message) -> bool:
+class AdminFilter:
+    """A filter to check if a user is an admin."""
+
+    @classmethod
+    def admin(cls) -> filters.Filter:
+        """
+        Creates a filter to check if a user is an admin.
+
+        Returns:
+            filters.Filter: A filter to check if a user is an admin.
+        """
+
+        async def func(flt: None, client: Client, message: Message) -> bool:  # noqa: ARG001
+            """
+            Checks if a user is an admin.
+
+            Parameters:
+                client (Client): The Pyrogram client.
+                message (Message): The message to check.
+
+            Returns:
+                bool: True if the user is an admin, False otherwise.
+            """
             user_id = message.from_user.id
-            global_mode = options.settings.GLOBAL_MODE
-            return user_id in config.ROOT_ADMINS_ID or (global_mode and allow_global)
+
+            if user_id in config.ROOT_ADMINS_ID:
+                return True
+            
+            # --- این بخش اصلاح شد تا دیگر پیام خطا ندهد ---
+            return False 
+            # --- پایان اصلاح ---
+
 
         return filters.create(func, "AdminFilter")
