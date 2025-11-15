08444ebb98e7352ac2db884db5417369276fe72e
landanfp <159138257+landanfp@users.noreply.github.com>
2025-11-15 11:37:51 +0330

    Update __init__.py

--- bot/utilities/pyrofilters/__init__.py
+++ bot/utilities/pyrofilters/__init__.py
@@ -1,13 +1,10 @@
-# bot/utilities/pyrofilters/__init__.py file :
-
 from .admins import AdminsFilter
 from .conversation import ConversationFilter, ConvoMessage
 from .subscription import SubscriptionFilter, SubscriptionMessage
 
 
-class PyroFilters:
-    """A class to hold all the custom filters."""
+class PyroFilters(AdminsFilter, SubscriptionFilter, ConversationFilter):
+    pass
 
-    admin = AdminsFilter.admin
-    convo = ConversationFilter.conversation
-    sub = SubscriptionFilter.subscription
+
+__all__ = ["ConvoMessage", "SubscriptionMessage"]
