bbb2173f6086e895df9c341c9ea6ed8bac9fa316
landanfp <159138257+landanfp@users.noreply.github.com>
2025-11-24 15:14:11 +0330

    Update admins.py

--- bot/utilities/pyrofilters/admins.py
+++ bot/utilities/pyrofilters/admins.py
@@ -6,7 +6,7 @@ from pyrogram.client import Client
 from pyrogram.types import Message
 
 from bot.config import config
-from bot.plugins.base.set import ADMIN  # تغییر: import ADMIN global
+# from bot.plugins.base.set import ADMIN  # فیکس: حذف import ADMIN برای جلوگیری از cyclic import
 from bot.options import options
 
 
@@ -18,6 +18,6 @@ class AdminsFilter:
         async def func(flt: None, client: Client, message: Message) -> bool:
             user_id = message.from_user.id
             global_mode = options.settings.GLOBAL_MODE
-            return user_id in ADMIN or (global_mode and allow_global)  # تغییر: ADMIN به جای config.ROOT_ADMINS_ID
+            return user_id in config.ROOT_ADMINS_ID or (global_mode and allow_global)  # فیکس: استفاده از config.ROOT_ADMINS_ID به جای ADMIN
 
         return filters.create(func, "AdminFilter")
