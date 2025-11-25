bc3269cc3f8a96205c50243f9cf2bb014ae26710
landanfp <159138257+landanfp@users.noreply.github.com>
2025-11-25 11:36:02 +0330

    Update ban.py

--- bot/plugins/moderation/ban.py
+++ bot/plugins/moderation/ban.py
@@ -14,13 +14,12 @@ from bot.database import MongoDB
 from bot.utilities.helpers import RateLimiter
 from bot.utilities.pyrofilters import ConvoMessage
 from bot.utilities.pyrotools import HelpCmd
-from bot.utilities.pyrofilters import PyroFilters
 
 database = MongoDB()
 
 
 @Client.on_message(
-    filters.private & PyroFilters.admin() & filters.command("ban"),
+    filters.private & filters.command("ban"),
 )
 @RateLimiter.hybrid_limiter(func_count=1)
 async def ban_user(client: Client, message: ConvoMessage) -> Message | None:
